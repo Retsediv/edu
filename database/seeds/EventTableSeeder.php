44561e7c8331583b0ab0fdad9a49112b93d259ab
vagrant <retsediv1@gmail.com>
2015-09-24 16:38:55 +0000

    Refactor seeding. Added new seeding classes. Setting email smtp server and mail sending, password resets. Delete registration and added views and routes for adding new school by other method.

---
+++ database/seeds/EventTableSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 24.09.15
+ * Time: 19:31
+ */
+class EventTableSeeder
+{
+
+}
---
+++ database/seeds/RoleUserTableSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: andrew
+ * Date: 24.09.15
+ * Time: 19:30
+ */
+class RoleUserTableSeeder
+{
+
+}
