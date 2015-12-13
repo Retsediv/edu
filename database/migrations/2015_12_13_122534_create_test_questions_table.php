f76b4428880e2d21357c09fc70bb93d27491eaf4
vagrant <vagrant@homestead>
2015-12-13 12:39:32 +0000

    Added testing system migrations.

---
+++ database/migrations/2015_12_13_122534_create_test_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_questions', function(Blueprint $table){
+            $table->increments('id');
+            $table->text('body');
+            $table->integer('test_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('test_questions');
+    }
+}
