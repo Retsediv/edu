de4a7937ac7c3920a90de536a1f5e34e33adc0a7
= <retsediv1@gmail.com>
2016-03-15 19:45:40 +0200

    Add 404 page.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style type="text/css">
+        body {color:#222; font-size:13px;font-family: sans-serif; background:#fff url("/images/404bg.png") left top repeat-x;}
+        h1 {font-size:300%;font-family:'Trebuchet MS', Verdana, sans-serif; color:#000}
+        #page {font-size:122%;width:720px; margin:144px auto 0 auto;text-align:left;line-height:1.2;}
+        #message {padding-right:400px;min-height:360px;background:transparent url("/images/404.png") right top no-repeat;}
+        a:visited {color: #000; text-decoration: none;}
+    </style>
+    <meta http-equiv="Content-Type" content="application/xhtml; charset=utf-8" />
+    <title>Помилка 404 – Сторінку не знайдено</title>
+</head>
+<body>
+<div id="page">
+    <div id="message">
+        <h1>404</h1>
+        <p>— Не те, щоб зовсім не попав,— сказал Пух,— але тільки не попав в шарік!</p>
+        <p><a href="{{ route('home') }}">Почнемо з початку...</a></p>
+    </div>
+</div>
+</body>
+</html>
